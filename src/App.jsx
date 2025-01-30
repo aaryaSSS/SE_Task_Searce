01d3f8ad65c68e31b7fe85f5a87fd0e0896a6dbc
Arkesh Choudhury <arkesh.choudhury@ad1.searceinc.it>
2025-01-30 18:57:10 +0530

    Auth Implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductListing from './components/ProductListing';
 import ProductDetails from './components/ProductDetails';
+import Login from './components/Login';
 import CartItems from './components/CartItems';
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<Login/>}/>
         <Route path="/" element={<ProductListing />} />
         <Route path="/product/:id" element={<ProductDetails />} />
         <Route path="/cart" element={<CartItems />} />
--- src/app/store.js
+++ src/app/store.js
@@ -2,15 +2,17 @@ import { configureStore } from '@reduxjs/toolkit';
 import productsReducer from '../features/products/productsSlice';
 import { productsApi } from '../api/productsApi';
 import cartReducer from '../features/cart/cartSlice';
+import {authApi} from '../api/authApi';
 
 export const store = configureStore({
   reducer: {
     products: productsReducer,
     [productsApi.reducerPath]: productsApi.reducer,
     cart: cartReducer,
+    [authApi.reducerPath]: authApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(productsApi.middleware),
+    getDefaultMiddleware().concat(authApi.middleware, productsApi.middleware),
 });
 
 export default store;
